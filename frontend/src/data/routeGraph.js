9ab58dc9e67d96d390ce797374e9e057f7fbb92d
Ghyfran <gelapmalam931@gmail.com>
2026-05-24 16:18:35 +0700

    update data nodes and edges

--- frontend/src/data/routeGraph.js
+++ frontend/src/data/routeGraph.js
@@ -1,26 +1,27 @@
 // Graf manual contoh awal. Sesuaikan koordinat node dan edge dengan hasil observasi lapangan.
 export const routeNodes = {
-  pelabuhan: { id: 'pelabuhan', name: 'Pelabuhan Penyengat', lat: 0.9259, lng: 104.4206 },
-  simpang_dermaga: { id: 'simpang_dermaga', name: 'Simpang Dermaga', lat: 0.9264, lng: 104.4209 },
-  tepi_laut: { id: 'tepi_laut', name: 'Tepi Laut Penyengat', lat: 0.9267, lng: 104.4196 },
-  masjid: { id: 'masjid', name: 'Masjid Raya Sultan Riau', lat: 0.9277, lng: 104.4209 },
-  engku_putri: { id: 'engku_putri', name: 'Makam Engku Putri', lat: 0.9286, lng: 104.4215 },
-  makam_rah: { id: 'makam_rah', name: 'Makam Raja Ali Haji', lat: 0.9292, lng: 104.4228 },
-  balai_adat: { id: 'balai_adat', name: 'Balai Adat Melayu', lat: 0.9272, lng: 104.4222 },
-  istana: { id: 'istana', name: 'Istana Kantor', lat: 0.9263, lng: 104.4233 },
-  simpang_bukit: { id: 'simpang_bukit', name: 'Simpang Bukit Kursi', lat: 0.9295, lng: 104.4213 },
-  benteng: { id: 'benteng', name: 'Benteng Bukit Kursi', lat: 0.9302, lng: 104.4205 }
+  pelabuhan: { id: 'pelabuhan', name: 'Pelabuhan Penyengat', lat: 0.930219, lng: 104.421951 },
+  masjid: { id: 'masjid', name: 'Masjid Raya Sultan Riau', lat: 0.929487, lng: 104.420334 },
+  makam_rah: { id: 'makam_rah', name: 'Makam Raja Ali Haji', lat: 0.927698, lng: 104.421374 },
+  balai_adat: { id: 'balai_adat', name: 'Balai Adat Melayu', lat: 0.927296, lng: 104.414701 },
+  istana: { id: 'istana', name: 'Istana Kantor', lat: 0.928224, lng: 104.418912 },
+  benteng: { id: 'benteng', name: 'Benteng Bukit Kursi', lat: 0.929366, lng: 104.417737 }
 }
 
 export const routeEdges = [
-  ['pelabuhan', 'simpang_dermaga'],
-  ['simpang_dermaga', 'tepi_laut'],
-  ['simpang_dermaga', 'masjid'],
-  ['masjid', 'engku_putri'],
-  ['engku_putri', 'makam_rah'],
+  ['pelabuhan', 'masjid'],
+  ['pelabuhan', 'makam_rah'],
+  ['pelabuhan', 'balai_adat'],
+  ['pelabuhan', 'istana'],
+  ['pelabuhan', 'benteng'],
+  ['masjid', 'makam_rah'],
   ['masjid', 'balai_adat'],
+  ['masjid', 'istana'],
+  ['masjid', 'benteng'],
+  ['makam_rah', 'balai_adat'],
+  ['makam_rah', 'istana'],
+  ['makam_rah', 'benteng'],
   ['balai_adat', 'istana'],
-  ['engku_putri', 'simpang_bukit'],
-  ['simpang_bukit', 'benteng'],
-  ['makam_rah', 'balai_adat']
+  ['balai_adat', 'benteng'],
+  ['istana', 'benteng'],
 ]
